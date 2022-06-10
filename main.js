83d6ad5703f9fa07324df66b8a5b2d0da492e258
José Aljuria <josejavieral13@gmail.com>
2022-06-10 17:18:04 -0400

    dragTask and removeTaskDroped in column class

--- main.js
+++ main.js
@@ -1,46 +1,3 @@
-document.querySelectorAll('.column').forEach(column => {
-	column.addEventListener('dragover', event => {
-		if (event.target.classList.contains('column')) {
-			event.preventDefault();
-		}
-	})// evita que se suelte sobre una task
-
-	column.addEventListener('drop', event => {
-		if (event.dataTransfer.effectAllowed === 'move') {
-			const template = document.getElementById('templateTask').content.cloneNode(true);
-			const task = JSON.parse(event.dataTransfer.getData('text'));
-			template.querySelector('.task__title').innerText = task.title
-			template.querySelector('.task__body').innerText = task.body;
-			event.target.appendChild(template);
-		}
-	})
-})
-
-
-function dragTask(event) {
-	const task = {
-		title: event.target.querySelector('.task__title').innerText,
-		body: event.target.querySelector('.task__body').innerText,
-	}
-
-
-	event.dataTransfer.effectAllowed = 'move';
-	event.dataTransfer.setData('text', JSON.stringify(task));
-}
-
-// remove task when task were droped exitily
-function removeTaskDroped(event) {
-
-	if (event.dataTransfer.dropEffect !== 'none') {
-		event.target.remove();
-	}
-}
-
-function newTask(event) {
-	const templateTask = document.getElementById('templateTask').content.cloneNode(true);
-	event.target.parentNode.appendChild(templateTask);
-}
-
 function edit(event) {
 	event.target.contentEditable = true;
 }
@@ -49,6 +6,96 @@ function save(event) {
 	event.target.contentEditable = false;
 }
 
-function deleteTask(event){
+function deleteTask(event) {
 	event.target.parentNode.parentNode.parentNode.remove()
 }
+
+function getRandomIntInclusive(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1) + min).toString(); //The maximum is inclusive and the minimum is inclusive
+}
+
+class Column extends HTMLElement {
+	tasks = [];
+
+	constructor() {
+		super();
+		this.render();
+		this.querySelector('.new_task').addEventListener('click', this.newTask.bind(this));
+		this.querySelectorAll('.column').forEach(column => {
+			column.addEventListener('dragover', event => {
+				if (event.target.classList.contains('column')) {
+					event.preventDefault();
+				}
+			})// evita que se suelte sobre una task
+
+			column.addEventListener('drop', event => {
+				if (event.dataTransfer.effectAllowed === 'move') {
+
+					const task = JSON.parse(event.dataTransfer.getData('text'));
+					this.#createTask(task);
+					this.tasks.push(task);
+				}
+			})
+		})
+	}
+
+	render() {
+		this.innerHTML = `
+		<section class="column">
+			<p class="new_task" onclick="">
+				Create Task
+			</p>
+		</section>`;
+
+	}
+
+	newTask(event) {
+		const task = { title: '', body: '', id: getRandomIntInclusive(1,100000000)};
+		this.#createTask(task);
+		this.tasks.push(task);
+		console.log(this.tasks)
+	}
+
+	#createTask({ id , title, body }) {
+		const templateTask = document.getElementById('templateTask').content.cloneNode(true); // clona template
+		
+		this.firstElementChild.appendChild(templateTask);// agrega a DOM
+		const newElementTask = this.firstElementChild.lastElementChild
+
+		// setting
+		newElementTask.dataset.id = id;
+		newElementTask.querySelector('.task__title').innerText = title
+		newElementTask.querySelector('.task__body').innerText = body;
+
+		// agrega eventos
+		newElementTask.addEventListener('dragstart', this.dragTask.bind(this));
+		newElementTask.addEventListener('dragend',this.removeTaskDroped.bind(this));
+
+		return newElementTask;
+
+	}
+
+	dragTask(event) {
+		const task = {
+			id: event.target.dataset.id,
+			title: event.target.querySelector('.task__title').innerText,
+			body: event.target.querySelector('.task__body').innerText,
+		}
+
+		event.dataTransfer.effectAllowed = 'move';
+		event.dataTransfer.setData('text', JSON.stringify(task));
+	}
+
+	// remove task when task were droped exitily
+	removeTaskDroped(event) {
+		
+	if (event.dataTransfer.dropEffect !== 'none') {
+		this.tasks = this.tasks.filter((task) => task.id !== event.target.dataset.id);
+		event.target.remove();
+		console.log(this.tasks)
+	}
+}
+}
+customElements.define('task-column', Column);
