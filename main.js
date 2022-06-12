5695cb5ceaf09128bbbc25031c27b4d1c5c63144
José Aljuria <josejavieral13@gmail.com>
2022-06-12 07:51:09 -0400

    add storage

--- main.js
+++ main.js
@@ -8,13 +8,107 @@ function getRandomIntInclusive(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min).toString(); //The maximum is inclusive and the minimum is inclusive
 }
 
+class Data{
+	#data = new Map();
+	#dataStore = null;
+
+	constructor(dataStore){
+		this.#dataStore = dataStore;
+	}
+
+	/**
+	 * @param {Iterable<readonly [any, any]>} data
+	 */
+	set data(data){
+		if(!Array.isArray(data))
+			return new Error('typeError: Data need array');
+
+		const newData = new Map();
+		data.forEach(task => newData.set(task.id, task));
+
+		this.#data = newData;
+	}
+
+	add(task){
+		this.#data.set(task.id, Object.assign({}, task));
+	}
+
+	remove(id){
+		return this.#data.delete(id);
+	}
+
+	find(id){
+		return Object.assign({}, this.#data.get(id));
+	}
+
+	getAll(){
+		return Array.from(this.#data.values());
+	}
+
+	update(task) {
+		if(this.#data.has(task.id)){
+			this.#data.set(task.id, task);
+			return true;
+		}
+
+		return false;
+	}
+
+	save(){
+		this.#dataStore.save();
+	}
+
+
+}
+
+class DataStore{
+	#data = [];
+	#store = null;
+	length = 0;
+
+	constructor(store){
+		this.#store = store;
+	}
+
+	attach(){
+		const newData = new Data(this);
+		this.#data[this.length++] = newData;
+
+		return newData;
+	}
+
+	save(){
+
+		const data = this.#data.map((item)=> item.getAll());
+
+		this.#store.setItem('data', JSON.stringify(data));
+	}
+
+	populate(){
+		let data = JSON.parse(this.#store.getItem('data'));
+
+		if(!Array.isArray(data))
+			return;
+		
+		for (let i = 0; i < data.length; i++) {
+			for (let j = 0; j < data[i].length; j++) {
+				this.#data[i].add(data[i][j]);
+			}
+			
+		}
+
+	}
+}
+
+var store = new DataStore(localStorage);
+
 class Column extends HTMLElement {
-	tasks = [];
+	tasks = null;
 
 	constructor() {
 		super();
+		this.tasks = store.attach();
 		this.render();
-
 	}
 
 	render() {
@@ -39,24 +133,25 @@ class Column extends HTMLElement {
 				if (event.dataTransfer.effectAllowed === 'move') {
 					const task = JSON.parse(event.dataTransfer.getData('text'));
 					this.#createTask(task);
-					this.tasks.push(task);
+					this.tasks.add(task);
+					this.tasks.save();
 				}
 			})
 		})
 
 		// setting tasks
-		this.tasks.forEach((task) => this.#createTask(task))
+
+		this.tasks.getAll().forEach((task) => this.#createTask(task));
 	}
 
 	newTask(event) {
 		const task = { title: '', body: '', id: getRandomIntInclusive(1, 100000000) };
 		this.#createTask(task);
-		this.tasks.push(task);
-		console.log(this.tasks)
+		this.tasks.add(task);
+		this.tasks.save();
 	}
 
 	#createTask({ id, title, body }) {
-		console.log({ id }, { title }, { body })
 		const templateTask = document.getElementById('templateTask').content.cloneNode(true); // clona template
 
 		this.firstElementChild.appendChild(templateTask);// agrega a DOM
@@ -73,27 +168,24 @@ class Column extends HTMLElement {
 		newElementTask.querySelector('.task__icon-delete').addEventListener('click', this.#deleteTask.bind(this, id));
 		newElementTask.querySelector('.task__title').addEventListener('blur', this.#saveTextChange.bind(this, [id, 'title']));
 		newElementTask.querySelector('.task__body').addEventListener('blur', this.#saveTextChange.bind(this, [id, 'body']));
-
 		return newElementTask;
 
 	}
 
 	#deleteTask(id) {
-		this.tasks = this.tasks.filter(task => task.id !== id);
+		this.tasks.remove(id);
+		this.tasks.save();
 		this.render();
 	}
 
 	#saveTextChange([id, text]) {
 		event.target.contentEditable = false;
 		const newText = event.target.innerText;
-
-		this.tasks.forEach(task => {
-			if (task.id === id) {
-				task[text] = newText;
-			}
-		})
-
-		console.log(this.tasks);
+	
+		const task = this.tasks.find(id);
+		task[text] = newText;
+		this.tasks.update(task);
+		this.tasks.save();
 	}
 
 	dragTask(event) {
@@ -111,9 +203,16 @@ class Column extends HTMLElement {
 	removeTaskDroped(event) {
 
 		if (event.dataTransfer.dropEffect !== 'none') {
-			this.tasks = this.tasks.filter((task) => task.id !== event.target.dataset.id);
+			this.tasks.remove(event.target.dataset.id);
 			event.target.remove();
+			this.tasks.save();
 		}
 	}
 }
+
 customElements.define('task-column', Column);
+
+window.addEventListener('DOMContentLoaded', ()=> {
+	store.populate();
+	document.querySelectorAll('task-column').forEach(column => column.render());
+});
