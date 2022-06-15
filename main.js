73e16e14fbeef678bfe66d8ad137da094329000d
José Aljuria <josejavieral13@gmail.com>
2022-06-15 12:44:40 -0400

    create saveTitle and getTitle functions

--- main.js
+++ main.js
@@ -215,4 +215,21 @@ customElements.define('task-column', Column);
 window.addEventListener('DOMContentLoaded', ()=> {
 	store.populate();
 	document.querySelectorAll('task-column').forEach(column => column.render());
+	document.querySelector('.title > input').value = getTitle();
 });
+
+function saveTitle(event){
+	const title = event.target.value;
+	if(!title)
+		return false;
+	
+	localStorage.setItem('title', JSON.stringify(title));
+}
+
+function getTitle(){
+	const title = localStorage.getItem('title');
+	if(!title)
+		return '';
+	
+	return JSON.parse(title);
+}
