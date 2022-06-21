b4cbe8ca00868c760c663128cec619ee492eae73
José Aljuria <josejavieral13@gmail.com>
2022-06-21 16:10:29 -0400

    stop bubble drop

--- src/js/Components/Separator.js
+++ src/js/Components/Separator.js
@@ -3,19 +3,60 @@ export default class Separator extends HTMLElement {
 	constructor(props) {
 		super(props);
 
+		this.attachShadow({ mode: 'open' })
+
 		this.addEventListener('dragenter', () => {
-			this.querySelector('.separator').classList.toggle('space', true);
+			this.shadowRoot.querySelector('.separator').classList.toggle('space', true)
 		});
 		this.addEventListener('dragleave', () => {
-			this.querySelector('.separator').classList.toggle('space', false);
+			this.shadowRoot.querySelector('.separator').classList.toggle('space', false);
 		});
+
+		this.addEventListener('dragover', (event) => {
+			event.preventDefault();
+		})
+
+		this.addEventListener('drop', event => {
+			event.stopPropagation();
+			if (event.dataTransfer.effectAllowed === 'move') {
+				const task = JSON.parse(event.dataTransfer.getData('text'));
+				this.shadowRoot.querySelector('.separator').classList.toggle('space', false);
+				const newEvent = new CustomEvent('dropTask', {
+					detail: {
+						task
+					},
+					composed: true
+				})
+				this.dispatchEvent(newEvent);
+
+			}
+		})
+
 		this.render();
 	}
 
 	render() {
-		this.innerHTML = `
+		this.shadowRoot.innerHTML = `
+			${this.#style()}
 			<div class="separator"></div>
 		`;
+	}
+
+	#style() {
+		return (`
+			<style>
+			.separator {
+				box-sizing: border-box;
+				border: 1px solid red;
+				padding: 1rem;
+			}
+			.space {
+				width: 100%;
+				height: 7rem;
+			}
+
+			</style>
+		`);
 	}
 }
 
