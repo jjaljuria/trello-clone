69822f9e24f5efa5f98fe575568a1c3a4c504ad6
José Aljuria <josejavieral13@gmail.com>
2022-06-30 09:46:54 -0400

    create column

--- src/js/main.js
+++ src/js/main.js
@@ -15,12 +15,43 @@ function getTitle() {
   const title = localStorage.getItem("title");
   if (!title) return "";
 
-  return JSON.parse(title);
+  return title;
+}
+
+function createColumn(event) {
+  event.preventDefault();
+  event.stopPropagation();
+  const column = event.target.parentNode;
+  console.log(column);
+  column.innerHTML = `
+  <form class="form-create-column">
+    <input type=text name="name">
+    <button type="submit">Create</button>
+  </form>
+  `;
+  column
+    .querySelector(".form-create-column")
+    .addEventListener("submit", instanceColumn);
+}
+
+function instanceColumn(event) {
+  event.preventDefault();
+
+  const newColumn = document.createElement("task-column");
+  const title = event.target.elements["name"].value;
+  newColumn.title = title;
+  const container = event.target.parentNode;
+
+  container.insertAdjacentElement("beforebegin", newColumn);
 }
 
 window.addEventListener("DOMContentLoaded", () => {
-  store.populate();
-  document.querySelectorAll("task-column").forEach((column) => column.render());
+  // store.populate();
+  // document.querySelectorAll("task-column").forEach((column) => column.render());
   document.querySelector(".title > input").value = getTitle();
   document.querySelector(".title > input").onblur = saveTitle;
+
+  document
+    .querySelector(".add-column .add-column__button")
+    .addEventListener("click", createColumn);
 });
